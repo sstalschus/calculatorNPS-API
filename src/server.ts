9129d5ab5e57792c255bfd19515bfa8a581bc3ce
Samuel Stalschus <samuel5stalschus@gmail.com>
2021-02-22 23:01:17 -0300

    update

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,14 @@
-import express from 'express'
+import express, { response } from 'express'
 
 const app = express()
 
+app.get("/users", (req, res) => {
+
+  return res.json({ message: "Hello Word - NLW04" })
+})
+
+app.post("/", (req, res) => {
+
+  return res.json({ message: "os dados foram salvos com sucesso" })
+})
 app.listen(3333, () => console.log("Server is running!"))
