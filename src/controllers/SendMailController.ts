e82250f0dad5dee406f4a269ecd538a4c7af08c4
Samuel Stalschus <samuel5stalschus@gmail.com>
2021-02-25 21:33:50 -0300

    finish sendMail

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -1,5 +1,6 @@
 import { Request, response, Response } from "express";
 import { getCustomRepository } from "typeorm";
+import { resolve } from 'path'
 import { SurveysRepository } from "../repositories/SurveysRepository";
 import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
 import { UsersRepository } from "../repositories/UsersRepository";
@@ -15,9 +16,9 @@ class SendMailController {
     const surveysRepository = getCustomRepository(SurveysRepository)
     const surveysUsersRepository = getCustomRepository(SurveysUsersRepository)
 
-    const userAlreadyExists = await usersRepository.findOne({email})
+    const user = await usersRepository.findOne({email})
 
-    if(!userAlreadyExists) {
+    if(!user) {
       return res.status(400).json({
         error: 'User does not exists!'
       })
@@ -30,17 +31,37 @@ class SendMailController {
         error: 'Survey does not exists!'
       })
     }
+    
+    const npsPath = resolve(__dirname, '..', 'views', 'emails', 'npsMail.hbs')
+    const variables = {
+      name: user.name,
+      title: survey.title,
+      description: survey.description,
+      user_id: user.id,
+      link: process.env.URL_MAIL
+    }
+
+    const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
+      where: [{user_id: user.id}, {value: null}],
+      relations: ['user', 'survey']
+    })
+
+    if(surveyUserAlreadyExists){
+      await SendMailService.execute(email, survey.title, variables, npsPath)
+      return res.json(surveyUserAlreadyExists)
+    }
 
     // salvar informações na table surveyUser
     const surveyUser = surveysUsersRepository.create({
-      user_id: userAlreadyExists.id,
+      user_id: user.id,
       survey_id
     })
     await surveysUsersRepository.save(surveyUser)
 
+
     // enviar email para o usuário
     
-    await SendMailService.execute(email, survey.title, survey.description)
+    await SendMailService.execute(email, survey.title, variables, npsPath)
 
     
 
